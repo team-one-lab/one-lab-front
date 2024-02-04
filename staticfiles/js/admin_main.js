c88ac923a2cd50821b115a5f7f97ebe0f93f0812
jo <doselaka7059@gmail.com>
2024-02-04 23:14:39 +0900

    관리자 페이지 퍼블리싱 + js 80% 완성

--- staticfiles/js/admin_main.js
+++ staticfiles/js/admin_main.js
@@ -1,58 +1,11 @@
-// function handleButtonClick(buttonText) {
-//     alert("Button Clicked: " + buttonText);
+/** 프로필 변경 js 기능 */
 
-//     // 기존 버튼 클래스 변경
-//     var blockContentButton = document.querySelector(".block-content");
-//     blockContentButton.classList.remove("block-content");
-//     blockContentButton.classList.add("block-content", "disabled");
-
-//     // 새로 클릭한 버튼 클래스 변경
-//     var blockContent1Button = document.querySelector(".block-content1");
-//     blockContent1Button.classList.remove("block-content1", "disabled");
-//     blockContent1Button.classList.add("block-content1");
-// }
-
-// function toggleDropdown() {
-//     var dropdown = document.getElementById("dropdownOptions");
-//     dropdown.classList.toggle("show");
-// }
-
-// function selectOption(option) {
-//     var selectedOption = document.querySelector(".surface-none span");
-//     selectedOption.textContent = option;
-//     toggleDropdown();
-// }
-
-// document.addEventListener("click", function (event) {
-//     var dropdown = document.getElementById("dropdownOptions");
-//     var button = document.querySelector(".surface-none");
-
-//     if (
-//         event.target !== button &&
-//         event.target.parentNode !== button &&
-//         event.target.parentNode.parentNode !== button
-//     ) {
-//         dropdown.classList.remove("show");
-//     }
-// });
-
+/** 버튼태그 클릭하면 드롭다운 메뉴가 나오는 js 기능 */
 function toggleDropdown(dropdownId) {
     var dropdownOptions = document.getElementById(dropdownId);
     dropdownOptions.style.display =
         dropdownOptions.style.display === "block" ? "none" : "block";
 }
-
-// function selectOption(option) {
-//     var selectedOption = document.querySelector("#one span");
-//     var selectedOption2 = document.querySelector("#two span");
-//     var selectedOption3 = document.querySelector("#three span");
-//     if (selectedOption || selectedOption2 || selectedOption3) {
-//         selectedOption.textContent = option;
-//         selectedOption2.textContent = option;
-//         selectedOption3.textContent = option;
-//     }
-//     toggleDropdown(); // 선택 후 드롭다운을 다시 닫습니다.
-// }
 
 function selectOption(option, buttonId) {
     var selectedOption = document.querySelector(`#${buttonId} span`);
