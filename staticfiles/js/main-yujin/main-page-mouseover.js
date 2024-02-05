98b3f9b1733b9302cd77a6bdc387eee48e0961d6
yjp0619 <lisajj0619@gmail.com>
2024-02-05 15:43:12 +0900

    init

---
+++ staticfiles/js/main-yujin/main-page-mouseover.js
@@ -0,0 +1,7 @@
+document.getElementsByClassName('.RecommendFundingWrap_googleTaggedButton').onmouseover = function() {
+    this.style.opacity = "0.3";
+}
+
+document.getElementsByClassName('.RecommendFundingWrap_googleTaggedButton').onmouseout = function() {
+    this.style.opacity = "1";
+}
