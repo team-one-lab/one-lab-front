118e068d1146bdf5fce6fe8f041d23c5f02fb700
yjp0619 <lisajj0619@gmail.com>
2024-02-12 02:20:22 +0900

    login 페이지

--- staticfiles/js/login-yujin/login.js
+++ staticfiles/js/login-yujin/login.js
@@ -8,6 +8,17 @@ inputs.forEach((input) => {
     });
     input.addEventListener("blur", (e) => {
         input.classList.toString("Input_error")
-        e.target.style.border = "1px solid red";
+        e.target.style.border = "1px solid #f66";
     })
 });
+
+const pwInputs = document.querySelectorAll(".Input-pw");
+
+pwInputs.forEach((pwInput) => {
+    pwInput.addEventListener("focus", (e) => {
+        e.target.style.border = "1px solid #00c4c4"
+    });
+    pwInput.addEventListener("blur", (e) => {
+        e.target.style.border = "1px solid #dde2e6"
+    })
+})
