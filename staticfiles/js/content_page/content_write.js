2d6d6b97fe408db756dc814c5ea26ce577b317cb
Y_Jin <155802538+lisajj0619@users.noreply.github.com>
2024-02-13 18:22:45 +0900

    Revert "중간작업 pr 입니다."

--- staticfiles/js/content_page/content_write.js
+++
@@ -1,6 +0,0 @@
-const selectbtn = document.querySelector(".nesty-input");
-const selectbox = document.getElementById("request_issue_type_select");
-const body = document.getElementById("hc-wysiwyg");
-
-body.contentEditable= true;
-
