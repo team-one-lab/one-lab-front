13e8aab67dc25a8767990d164950ef0e73402819
SOYOUNGdev <soyoungim.sy@gmail.com>
2024-02-15 01:40:58 +0900

    자료 공유 목록 페이지 모바일 작업중

---
+++ staticfiles/js/share-app-soyoung/share-list-app.js
@@ -0,0 +1,121 @@
+const goTopButton = document.querySelector('.float-top-btn')
+
+// 마우스 휠을 위 방향으로 돌릴 때, 상향 버튼 나타나기
+window.addEventListener('wheel', (e) => { 
+    // e.deltaY가 양수면 down wheel, 음수면 up wheel
+    // e.deltaX가 양수면, 오른쪽 음수면 왼쪽
+    // console.log(e.deltaY, e.deltaX);
+    if(e.deltaY < 0) {
+        goTopButton.classList.add('active')
+    }else {
+        goTopButton.classList.remove('active')
+    }
+});
+
+// 위로가기 버튼 클릭 시 맨 위로 이동
+const scroll = () => {
+    if (window.scrollY !== 0) {
+        setTimeout(() => {
+            window.scrollTo(0, window.scrollY - 50);
+            scroll();
+        }, 10);
+    }
+}
+goTopButton.addEventListener('click', scroll)
+
+
+// tab버튼 쪽 학과명 클릭
+const tabButtons = document.querySelectorAll(".image-tab")
+
+tabButtons.forEach((tabButton) => {
+    tabButton.addEventListener("mouseover", function(e) {
+        clickTab = e.target.querySelector(".tab-label-text")
+        clickTab.classList.add('hover')
+    })
+    tabButton.addEventListener("mouseout", function(e) {
+        clickTab = e.target.querySelector(".tab-label-text")
+        clickTab.classList.remove('hover')
+    })
+    tabButton.addEventListener("click", function(e) {
+        tabButtons.forEach((tabButton) => {
+            clickTab = tabButton.querySelector(".tab-label-text")
+            clickTabImg = tabButton.querySelectorAll(".tab-thumbnail")
+            tabButton.classList.remove('active')
+            clickTab.classList.remove('active')
+            clickTabImg[1].classList.add('img-hide')
+        })
+        clickTab = e.target.querySelector(".tab-label-text")
+        clickTabImg = tabButton.querySelectorAll(".tab-thumbnail")
+        console.log(clickTabImg)
+        e.target.classList.add('active')
+        clickTabImg[1].classList.remove('img-hide')
+    })
+})
+
+// 좋아요 버튼
+const buttons = document.querySelectorAll('.like-button')
+buttons.forEach((button) => {
+    button.addEventListener('click', function(e) {
+        e.target.classList.toggle('active')
+    })
+})
+
+// 모달 확장 버튼 클릭 시, 서브 메뉴나오고 화살표 180도 회전
+const expandLists = document.querySelectorAll('.list-item')
+expandLists.forEach((expandList) => {
+    expandList.addEventListener('click', function(e) {
+        const expandBtn = expandList.children
+        const subMenu = expandBtn[1].parentElement.nextElementSibling
+        expandBtn[1].classList.toggle('active')
+        subMenu.classList.toggle('active')
+    })
+})
+
+
+// 닫기 버튼 요소를 가져옵니다.
+const closeButton = document.querySelector('.close-icon-wrapper');
+
+// 모달 오버레이 요소를 가져옵니다.
+const modalOverlay = document.querySelector('.bottom-modal-overlay');
+
+// 모달 컨텐츠 요소를 가져옵니다.
+const modalContent = document.querySelector('.bottom-modal-content');
+
+// 초기에 모달을 숨기는 함수를 정의합니다.
+function hideModal() {
+    // 모달 오버레이의 투명도를 0으로 설정합니다.
+    modalOverlay.style.opacity = '0';
+    // 모달 오버레이의 z-index를 0으로 설정합니다.
+    modalOverlay.style.zIndex = '0';
+    // 모달 컨텐츠의 위치를 아래로 이동하여 숨깁니다.
+    modalContent.style.transform = 'translateY(100%)';
+}
+
+// 초기에 모달을 숨깁니다.
+hideModal();
+
+// 닫기 버튼 클릭 시 동작할 함수를 정의합니다.
+function closeModal() {
+    // 모달을 숨깁니다.
+    hideModal();
+}
+
+// 닫기 버튼에 클릭 이벤트 리스너를 추가합니다.
+closeButton.addEventListener('click', closeModal);
+
+// 모바일 링크 요소를 가져옵니다.
+const mobileLink = document.querySelector('div.mobile-link');
+
+// 모달을 열고 닫는 함수를 정의합니다.
+function toggleModal() {
+    // 모달 오버레이와 모달 컨텐츠를 보이게 합니다.
+    modalOverlay.style.opacity = '1';
+    modalOverlay.style.zIndex = '9998';
+    modalContent.style.transform = 'translateY(0)';
+}
+
+// 모바일 링크를 클릭하면 모달을 엽니다.
+mobileLink.addEventListener('click', toggleModal);
+
+
+
