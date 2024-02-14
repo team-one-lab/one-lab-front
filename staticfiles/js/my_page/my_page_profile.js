39932ce73ca8d2e5a90024eae27850c0b0e36a13
Respec-Do <kanghyundo@gmail.com>
2024-02-14 15:54:33 +0900

    클래스명 수정완료+ 중간작업완료

--- staticfiles/js/my_page/my_page_profile.js
+++ staticfiles/js/my_page/my_page_profile.js
@@ -1,8 +1,9 @@
-const input = document.querySelector("#changeProfileImg")
+const input = document.getElementById("attach");
 const thumbnail = document.querySelector("div.image")
-const cancel = document.getElementById("btn_deletePhoto")
 const input2 = document.querySelector("#btn_updatePhoto")
-// console.log(input2)
+const cancel = document.getElementById("btn_deletePhoto")
+
+// // console.log(input2)
 
 input.addEventListener("change", (e) => {
     const [file] = e.target.files;
@@ -10,11 +11,10 @@ input.addEventListener("change", (e) => {
     reader.readAsDataURL(file);
     reader.addEventListener("load", (e) => {
         const path = e.target.result;
-        // console.log(path);
         if (path.includes("image")) {
             thumbnail.style.backgroundImage = `url(${path})`;
         } else {
-            thumbnail.style.backgroundImage = `url('images/attach.png')`;
+            thumbnail.style.backgroundImage = `url('https://static.wadiz.kr/assets/icon/profile-icon-1.png')`;
         }
     })
 })
@@ -28,12 +28,20 @@ cancel.addEventListener("click", (e) => {
     input.value = "";
 })
 
+// 확인 모달창 기능 구현
+const confirm_btn = document.getElementById("alertify-o-ok");
+const confirm_modal = document.getElementById("alertify-o");
+const confirm_modal_layout = document.getElementById("confirm-modal");
+const open_confirm_modal_btn = document.querySelector("ul li .btn-mint");
 
-// const label_checkboxes = document.querySelectorAll(".input-check");
-// console.log(label_checkboxes);
-// label_checkboxes.forEach(label_checkbox => {
-//     label_checkbox.addEventListener("click", (e) => {
-//         const checkbox = document.querySelector('input[type="checkbox"]');
-//         checkbox.innerHTML = `#interest_1::before` 
-//     })
-// })
+// 닫기 기능
+confirm_btn.addEventListener("click", () => {
+    confirm_modal.style.display="none";
+    confirm_modal_layout.style.display="none";
+})
+
+// 열기 기능
+open_confirm_modal_btn.addEventListener("click" , ()=> {
+    confirm_modal_layout.style.display="block";
+    confirm_modal.style.display="block";
+})
--- staticfiles/js/my_page/my_page_writelist.js
+++ staticfiles/js/my_page/my_page_writelist.js
@@ -1,12 +1,12 @@
 function myFunction() {
     // console.log(1+1);
-    document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("my-dropdown").classList.toggle("show");
   }
   
-  // Close the dropdown menu if the user clicks outside of it
+
   window.onclick = function(event) {
     if (!event.target.matches('.select-menu-hidden-input-box')) {
-      var dropdowns = document.getElementsByClassName("select-menu__menu-list");
+      var dropdowns = document.getElementsByClassName("select-menu-menu-list");
       var i;
       for (i = 0; i < dropdowns.length; i++) {
         var openDropdown = dropdowns[i];
@@ -17,16 +17,16 @@ function myFunction() {
     }
   }
 
-const mainText = document.querySelector(".select-menu__single-value")
-const options = document.querySelectorAll(".select-menu__option")
+const mainText = document.querySelector(".select-menu-single-value")
+const options = document.querySelectorAll(".select-menu-option")
 console.log(options);
 options.forEach(option => {
 option.addEventListener("click", function() {
     options.forEach(option => {
-        option.classList.remove("select-menu__option--is-selected")
+        option.classList.remove("select-menu-option--is-selected")
     });
-    this.classList.add("select-menu__option--is-selected");
+    this.classList.add("select-menu-option--is-selected");
     mainText.textContent = this.textContent;
-    document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("my-dropdown").classList.toggle("show");
 })
 })
