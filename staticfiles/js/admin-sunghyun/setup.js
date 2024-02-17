b2d91dee7b0f7348b0b671f57b864ba9750a7042
jo <doselaka7059@gmail.com>
2024-02-17 14:47:45 +0900

    저장1

---
+++ staticfiles/js/admin-sunghyun/setup.js
@@ -0,0 +1,18 @@
+const labels = Utils.months({ count: 7 });
+const data = {
+    labels: labels,
+    datasets: [
+        {
+            label: "Dataset 1",
+            data: Utils.numbers(NUMBER_CFG),
+            borderColor: Utils.CHART_COLORS.red,
+            backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
+        },
+        {
+            label: "Dataset 2",
+            data: Utils.numbers(NUMBER_CFG),
+            borderColor: Utils.CHART_COLORS.blue,
+            backgroundColor: Utils.transparentize(Utils.CHART_COLORS.blue, 0.5),
+        },
+    ],
+};
