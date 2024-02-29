b918144f76d98872e71e04c8e90c6fb82a96a9c0
jo <doselaka7059@gmail.com>
2024-02-29 10:46:34 +0900

    다시 수정

--- staticfiles/js/notify-sunghyun/notify.js
+++ staticfiles/js/notify-sunghyun/notify.js
@@ -12,19 +12,19 @@ active2.addEventListener("click", (e) => {
 
     active1.style.color = "#000";
     active1.style.borderBottom = "none";
-    active1.style.fontWeight = "700";
+    active1.style.fontWeight = "400";
 
     active3.style.color = "#000";
     active3.style.borderBottom = "none";
-    active3.style.fontWeight = "700";
+    active3.style.fontWeight = "400";
 
     active4.style.color = "#000";
     active4.style.borderBottom = "none";
-    active4.style.fontWeight = "700";
+    active4.style.fontWeight = "400";
 
     active5.style.color = "#000";
     active5.style.borderBottom = "none";
-    active5.style.fontWeight = "700";
+    active5.style.fontWeight = "400";
 });
 active1.addEventListener("click", (e) => {
     e.preventDefault();
@@ -34,19 +34,19 @@ active1.addEventListener("click", (e) => {
 
     active2.style.color = "#000";
     active2.style.borderBottom = "none";
-    active2.style.fontWeight = "700";
+    active2.style.fontWeight = "400";
 
     active3.style.color = "#000";
     active3.style.borderBottom = "none";
-    active3.style.fontWeight = "700";
+    active3.style.fontWeight = "400";
 
     active4.style.color = "#000";
     active4.style.borderBottom = "none";
-    active4.style.fontWeight = "700";
+    active4.style.fontWeight = "400";
 
     active5.style.color = "#000";
     active5.style.borderBottom = "none";
-    active5.style.fontWeight = "700";
+    active5.style.fontWeight = "400";
 });
 active3.addEventListener("click", (e) => {
     e.preventDefault();
@@ -56,19 +56,19 @@ active3.addEventListener("click", (e) => {
 
     active2.style.color = "#000";
     active2.style.borderBottom = "none";
-    active2.style.fontWeight = "700";
+    active2.style.fontWeight = "400";
 
     active1.style.color = "#000";
     active1.style.borderBottom = "none";
-    active1.style.fontWeight = "700";
+    active1.style.fontWeight = "400";
 
     active4.style.color = "#000";
     active4.style.borderBottom = "none";
-    active4.style.fontWeight = "700";
+    active4.style.fontWeight = "400";
 
     active5.style.color = "#000";
     active5.style.borderBottom = "none";
-    active5.style.fontWeight = "700";
+    active5.style.fontWeight = "400";
 });
 active4.addEventListener("click", (e) => {
     e.preventDefault();
@@ -78,40 +78,40 @@ active4.addEventListener("click", (e) => {
 
     active1.style.color = "#000";
     active1.style.borderBottom = "none";
-    active1.style.fontWeight = "700";
+    active1.style.fontWeight = "400";
 
     active2.style.color = "#000";
     active2.style.borderBottom = "none";
-    active2.style.fontWeight = "700";
+    active2.style.fontWeight = "400";
 
     active3.style.color = "#000";
     active3.style.borderBottom = "none";
-    active3.style.fontWeight = "700";
+    active3.style.fontWeight = "400";
 
     active5.style.color = "#000";
     active5.style.borderBottom = "none";
-    active5.style.fontWeight = "700";
+    active5.style.fontWeight = "400";
 });
 
 active5.addEventListener("click", (e) => {
     e.preventDefault();
     active5.style.borderBottom = "3px solid #008244";
     active5.style.color = "#00704A";
-    active5.style.fontWeight = "700";
+    active5.style.fontWeight = "400";
 
     active1.style.color = "#000";
     active1.style.borderBottom = "none";
-    active1.style.fontWeight = "700";
+    active1.style.fontWeight = "400";
 
     active2.style.color = "#000";
     active2.style.borderBottom = "none";
-    active2.style.fontWeight = "700";
+    active2.style.fontWeight = "400";
 
     active3.style.color = "#000";
     active3.style.borderBottom = "none";
-    active3.style.fontWeight = "700";
+    active3.style.fontWeight = "400";
 
     active4.style.color = "#000";
     active4.style.borderBottom = "none";
-    active4.style.fontWeight = "700";
+    active4.style.fontWeight = "400";
 });
